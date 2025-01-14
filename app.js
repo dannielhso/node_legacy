0a6f7fe0ff0e1bf3601c138e2aed49d1219846b4
dannielhso <dannielha@naver.com>
2025-01-14 16:34:39 +0900

    feat : 문의 목록 조회 기능 추가

--- app.js
+++ app.js
@@ -38,7 +38,6 @@ connectionPool.getConnection((err, connection) => {
     }
 });
 
-
 app.get('/', (req, res) => {
     res.render('index.ejs');  // .ejs 확장자를 붙여도 되고 없애도 된다.
 });
@@ -74,6 +73,21 @@ app.post('/api/contact', (req, res) => {
         }
     });
 });
+
+app.get('/contactList', (req, res) => {
+    const selectQuery = 'SELECT * FROM contact ORDER BY ID DESC';
+
+    connectionPool.query(selectQuery, (err, result) => {
+        if (err) {
+            console.error('데이터 조회 중 에러 발생 : ', err);
+            res.status(500).send('내부 서버 오류');
+        } else {
+            console.log('데이터가 조회 되었습니다.');
+            console.log(result);
+            res.render('contactList', {lists: result}); // ('뷰', {모델})
+        }
+    });
+})
 
 app.listen(port, () => {
     console.log(`Node Legacy App listening on port ${port}`);
