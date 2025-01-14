851e7d0d002e6d5b91b29f56474e53a0b13c780c
dannielhso <dannielha@naver.com>
2025-01-14 16:17:12 +0900

    feat : 문의사항 작성 기능 추가 (MySQL 커넥션 풀로 변경)

--- app.js
+++ app.js
@@ -14,15 +14,30 @@ app.use(express.static(__dirname+'/public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse JSON
 app.use(bodyParser.json())
-// MySQL Connection
-const connection = mysql.createConnection({ // 민감 정보 -> push 찍으면 안된다.
+
+// MySQL ConnectionPool :
+// MySQL 커넥션을 사용 할 때는, 주로 커넥션 풀을 이용하여 관리하는 것이 권장된다.
+const connectionPool = mysql.createPool({ // 민감 정보 -> push 찍으면 안된다.
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PW,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
+    connectionLimit: 10, // 최대 연결 수 설정(필요시)
     insecureAuth: true,
 }); // 어떻게 숨길 수 있을까? -> dotenv를 통해서 우회접근하고 .env는 추적 비활성화 한다.
+
+
+// MySQL connection check
+connectionPool.getConnection((err, connection) => {
+    if (err) {
+        console.error('MySQL 연결 중 에러 발생 : ', err);
+    } else {
+        console.log('MySQL에 연결되었습니다.');
+        connection.release();
+    }
+})
+
 
 app.get('/', (req, res) => {
     res.render('index.ejs')  // .ejs 확장자를 붙여도 되고 없애도 된다.
@@ -46,9 +61,18 @@ app.post('/api/contact', (req, res) => {
     const email = req.body.email;
     const memo = req.body.memo;
 
-    const data = `${name}, ${phone}, ${email}, ${memo}`
-
-    res.send(data)
+    const SQL_Query = `INSERT INTO contact(name, phone, email, memo, create_at, modify_at) 
+                    VALUES ('${name}', '${phone}', '${email}' ,'${memo}', NOW(), NOW() )`;
+    
+    connectionPool.query(SQL_Query, (err, result) => {
+        if (err) {
+            console.error('데이터 삽입 중 에러 발생 : ', err);
+            res.status(500).send('내부 서버 오류');
+        } else {
+            console.log('데이터가 삽입 되었습니다.');
+            res.send("<script>alert('문의사항이 등록되었습니다. '); location.href='/'</script>")
+        }
+    })
 })
 
 app.listen(port, () => {
