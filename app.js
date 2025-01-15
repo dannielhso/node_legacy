11021245c56ce3fc45068c2ebe309333247339a2
dannielhso <dannielha@naver.com>
2025-01-15 11:05:32 +0900

    fix : 오타 수정

--- app.js
+++ app.js
@@ -89,17 +89,17 @@ app.get('/contactList', (req, res) => {
     });
 });
 
-app.delete('/api/contactDelete/:id', (req, res) => { // path variable 방식으로 접근.
+app.post('/api/contactDelete/:id', (req, res) => {
     const id = req.params.id;
-    const deleteQuery = `DELETE FROM contact WHERE ID = '${id}'`;
+    const deleteQuery = `DELETE FROM contact WHERE ID='${id}'`;
     connectionPool.query(deleteQuery, (err, result) => {
         if (err) {
-            console.error('데이터 삭제 중 에러 발생 : ', err);
+            console.error('데이터 삭제 중 에러 발생:', err);
             res.status(500).send('내부 서버 오류');
         } else {
-            console.log('데이터가 삭제 되었습니다.');
+            console.log('데이터가 삭제되었습니다.');
             console.log(result);
-            res.send("<script>alert('문의사항이 삭제되었습니다. '); location.href='/contactList'</script>");
+            res.send("<script>alert('문의사항이 삭제되었습니다.'); location.href='/contactList'</script>");
         }
     });
 });
